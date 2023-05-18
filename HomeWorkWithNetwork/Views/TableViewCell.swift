539466cd1d9457b7cf82091d6385307b51083775
Elisey <gradelevelisey@mail.ru>
2023-05-18 15:06:15 +0300

    finish with TableViewCell

--- HomeWorkWithNetwork/Views/TableViewCell.swift
+++ HomeWorkWithNetwork/Views/TableViewCell.swift
@@ -9,5 +9,28 @@ import UIKit
 
 final class TableViewCell: UITableViewCell {
     
+    //MARK: - IBOutlets
+    @IBOutlet weak var characterImage: UIImageView! {
+        didSet {
+            characterImage.layer.cornerRadius = characterImage.frame.height / 2
+        }
+    }
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    private let networkManager = NetworkManager.shared
+    
+    //MARK: - Public methpds
+    func configure(with character: Character?) {
+        guard let character else { return }
+        nameLabel.text = character.name
+        networkManager.fetchImage(from: character.imageUrl) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.characterImage.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
     
 }
